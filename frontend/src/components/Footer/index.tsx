25410799a0ce2ddd4ad0c5aca3ec1f090a93d4cd
Rodrigoxalmeida <rodrigox.almeida@yahoo.com.br>
2021-05-07 00:16:33 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/Rodrigoxalmeida" target="_blank" rel="noreferrer">Rodrigo de Almeida Pessanha Salvador</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="linkedin.com/in/rodrigo-almeida-b6a99b177" target="_blank" rel="noreferrer">@Rodrigoxalmeida</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
